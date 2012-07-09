007ff650f5582c81d02734c8d4eb0b596a78148d
Edward Raison <eraison@Edwards-MacBook-Pro.local>
2012-07-08 22:26:26 -0400

    initial commit

---
+++ db/seeds/05_default_users.rb
@@ -0,0 +1,7 @@
+Profile.create!(:username => 'cnixon', 
+                :email => 'clinton.nixon@viget.com',
+                :display_name => "Clinton R. Nixon",
+                :password => 'password',
+                :password_confirmation => 'password',
+                :city => 'Durham',
+                :tos_accepted => '1')
